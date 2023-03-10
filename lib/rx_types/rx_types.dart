85dd96888e15990d1eff31ff95e29b5b14157278
elliotkhd <elliotkhd@gmail.com>
2023-03-10 14:41:48 +0800

    init commit

---
+++ lib/rx_types/rx_types.dart
@@ -0,0 +1,17 @@
+library rx_types;
+
+import 'dart:async';
+import 'dart:collection';
+
+import 'package:flutter/foundation.dart';
+import 'package:statex/rx_stream/get_stream.dart';
+import 'package:statex/rx_typedefs.dart';
+
+part 'rx_core/rx_impl.dart';
+part 'rx_core/rx_interface.dart';
+part 'rx_core/rx_num.dart';
+part 'rx_core/rx_string.dart';
+
+part 'rx_iterables/rx_list.dart';
+part 'rx_iterables/rx_set.dart';
+part 'rx_iterables/rx_map.dart';
---
+++ lib/statex.dart
@@ -0,0 +1,4 @@
+library statex;
+
+export 'rx_types/rx_types.dart';
+export 'rx_widget/rx_obx_widget.dart';
