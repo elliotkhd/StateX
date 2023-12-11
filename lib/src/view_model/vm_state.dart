7caca970148e7ffaa0001b7add0884d730014938
elliot <elliotkhd@gmail.com>
2023-12-11 22:05:43 +0800

    change createViewModel in VMState return type from subclasses to ViewModel

--- lib/src/view_model/vm_state.dart
+++ lib/src/view_model/vm_state.dart
@@ -3,12 +3,12 @@ part of 'view_model.dart';
 abstract class VMState<T extends StatefulWidget, VM extends ViewModel>
     extends State<T> {
   /// [viewModel] instance.
-  late final VM viewModel = createViewModel();
+  late final VM viewModel = createViewModel() as VM;
 
   /// create [viewModel] instance.
   ///
   /// must be override.
-  VM createViewModel();
+  ViewModel createViewModel();
 
   @override
   void initState() {
