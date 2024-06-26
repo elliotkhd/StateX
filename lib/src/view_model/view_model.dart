b81abe5ab39cb1c515b3d3d8d0a81ec37125a093
elliot <elliotkhd@gmail.com>
2024-06-26 11:25:55 +0800

    feat: bind widget and viewModel more tightly

--- lib/src/view_model/view_model.dart
+++ lib/src/view_model/view_model.dart
@@ -5,6 +5,11 @@ part 'vm_state.dart';
 
 abstract class ViewModel<T extends Widget> {
   late T widget;
+  bool initiated = false;
+  bool closed = false;
+  final Type typeOfWidget;
+
+  ViewModel() : typeOfWidget = T;
 
   void onInit() {}
 
--- lib/src/view_model/vm_state.dart
+++ lib/src/view_model/vm_state.dart
@@ -13,15 +13,19 @@ abstract class VMState<T extends StatefulWidget, VM extends ViewModel>
   @override
   void initState() {
     super.initState();
+    assert(vm.typeOfWidget == T, 'Widget Type in $VM should be $T');
+    assert(vm.initiated == false, 'ViewModel already initiated');
     vm.widget = widget;
     vm.onInit();
-    SchedulerBinding.instance
-        .addPostFrameCallback((timeStamp) => vm.onReady());
+    vm.initiated = true;
+    SchedulerBinding.instance.addPostFrameCallback((timeStamp) => vm.onReady());
   }
 
   @override
   void dispose() {
     super.dispose();
+    assert(vm.closed == false, 'ViewModel already disposed');
     vm.onClose();
+    vm.closed = true;
   }
 }
